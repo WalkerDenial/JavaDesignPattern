2fea8dfc2411feae599c34b42e39239ff381f5c2
Walker Denial <WalkerDenial@qq.com>
2020-12-18 23:22:50 +0800

    edit code

---
+++ src/com/walker/pattern/creational/factory/simple/FactoryTest.java
@@ -0,0 +1,58 @@
+package com.walker.pattern.creational.factory.simple;
+
+public class FactoryTest {
+
+    /**
+     * 测试场景，吃水果
+     *
+     * @param args
+     */
+    public static void main(String[] args) {
+
+    }
+
+    /**
+     * 没有使用工厂模式
+     */
+    private static void normalEat() {
+        // Jack 吃苹果
+        Apple jackApple = new Apple(); // Step.1 创建苹果
+        jackApple.eat(); // Step.2 吃苹果
+
+        // Tom 吃苹果
+        Apple tomApple = new Apple(); // Step.1 创建苹果
+        tomApple.eat(); // Step.2 吃苹果
+
+        // Anna 吃苹果
+        Apple annaApple = new Apple(); // Step.1 创建苹果
+        annaApple.eat(); // Step.2 吃苹果
+
+        /**
+         * 有没有发现问题？
+         * 1. 要吃个水果还要自己重水果;
+         * 2. 其他人不知道有没有苹果这类水果，自己也会去新建一个，那么就会出现重复；
+         * 3. 如果水果获取或打开的时候过于复杂，比如椰子，那不是要疯了；
+         * 4. 写成单例能解决重复的问题，但是别人由于不知情，破坏掉了对象呢？维护成本很大；
+         */
+    }
+
+    /**
+     * 使用工厂模式
+     */
+    private static void factoryEat() {
+        /**
+         * 1。告诉工厂我需要什么水果就行了，而不需要自己创建一个果树
+         * 2. 不需要维护水果树，由工厂统一管理
+         * 3. 哪怕创建对象很复杂，也不需要关系，由工厂负责处理，降低使用成本
+         */
+        Fruit jackApple = FruitFactory.getFruit(FruitFactory.TYPE_APPLE);
+        jackApple.eat();
+
+        Fruit tomApple = FruitFactory.getFruit(FruitFactory.TYPE_APPLE);
+        tomApple.eat();
+
+        Fruit annaApple = FruitFactory.getFruit(FruitFactory.TYPE_APPLE);
+        annaApple.eat();
+    }
+
+}
---
+++ src/com/walker/pattern/creational/factory/simple/FruitFactory.java
@@ -0,0 +1,30 @@
+package com.walker.pattern.creational.factory.simple;
+
+/**
+ * 水果工厂
+ */
+public class FruitFactory {
+
+    public static final int TYPE_APPLE = 1;
+    public static final int TYPE_ORANGE = 2;
+
+    private static Apple apple;
+    private static Orange orange;
+
+    /**
+     * 获取水果
+     *
+     * @param type
+     * @return
+     */
+    public static Fruit getFruit(int type) {
+        if (type == TYPE_APPLE) {
+            if (apple == null) apple = new Apple();
+            return apple;
+        } else if (type == TYPE_ORANGE) {
+            if (orange == null) orange = new Orange();
+            return orange;
+        } else return null;
+    }
+
+}
