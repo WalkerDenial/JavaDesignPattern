1f2320f81f60cc5934baa94689e05439bbabe007
Walker Denial <WalkerDenial@qq.com>
2020-12-19 21:51:38 +0800

    edit code

---
+++ src/com/walker/pattern/creational/factory/normal/source/BaseFruitFactory.java
@@ -0,0 +1,16 @@
+package com.walker.pattern.creational.factory.normal.source;
+
+import com.walker.pattern.creational.factory.simple.source.Fruit;
+
+/**
+ * 工厂基类
+ *
+ * @Author Walker Denial
+ * @Email WalkerDenial@gmail.com
+ * @Time Created at 2017/3/14 14:30.
+ */
+public interface BaseFruitFactory {
+
+    Fruit getFruit();
+
+}
